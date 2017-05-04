746c79d46994774ddb5797409712138952ad4d84
Ian Mobbs <ianmobbs@gmail.com>
2017-05-04 11:30:41 -0500

    Added points to plot, can't color tho

--- 02 Shiny/server.R
+++ 02 Shiny/server.R
@@ -171,6 +171,7 @@ shinyServer(function(input, output) {
   output$nonaggplot <- renderPlotly({
     p <- ggplot(nonaggdf()) +
       geom_boxplot(aes(x=Format, y=KPI, color=KPI)) +
+      geom_point(aes(x=Format, y=KPI)) +
       ylim(0, 6) +
       theme(axis.text.x=element_text(angle=90, size=10, vjust=0.5))
     ggplotly(p)
